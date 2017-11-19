81a24099b9224b61e50869af7024785a9edad8f7
rrsilaya <rrsilaya@up.edu.ph>
2017-11-19 12:21:43 +0800

    Add ingame middleware

--- src/router.js
+++ src/router.js
@@ -1,15 +1,28 @@
 import { Router } from 'express';
 
+import playerRouter from './entities/player/router';
 import questionRouter from './entities/question/router';
 import scoreRouter from './entities/score/router';
 import categoryRouter from './entities/category/router';
-import playerRouter from './entities/player/router';
 
 const router = Router();
+
+router.use(playerRouter);
+
+/* Middleware */
+router.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+
+  res.status(403).json({
+    status: 403,
+    message: 'You are not allowed to access this while in-game'
+  });
+});
 
 router.use(questionRouter);
 router.use(scoreRouter);
 router.use(categoryRouter);
-router.use(playerRouter);
 
 export default router;
